1be73eee2e553b9ef8dfd2eaaf8281a766dc7978
Killian Le Clainche <kleclain@purdue.edu>
2017-09-27 14:53:08 -0500

    Added new library JQuery-color for background color change. Created and added the logo.png. Login.js performs color animation and very rough skeleton of the login form that is dynamically created. stylesheet.css placed for enforcing standards.

--- source/javascripts/source/views/login.js
+++ source/javascripts/source/views/login.js
@@ -3,6 +3,25 @@ import * as form from "../forms/form.js";
 const loginView = {
 
 	init() {
+		const body = $("body");
+
+		body.append($("<div class='login--container'>" +
+			"<img src='/images/logo.png' alt='VENote' class='login--logo-image' />" +
+			"<div class='login--signin-form'>" +
+			"<span class='login--signin--header'>LOGIN</span>" +
+			"<div class='login--signin--login'></div>" +
+			"<div class='login--signin--register'></div>" +
+			"</div>" +
+			"<div class='login--register-form'>" +
+			"<div class='login--register--header'></div>" +
+			"<div class='login--register--register'></div>" +
+			"<div class='login--register--back'></div>" +
+			"</div>" +
+			"</div>"));
+
+		body.animate({"background-color": "#557FBE"}, 200, function() {
+
+		});
 
 	},
 
