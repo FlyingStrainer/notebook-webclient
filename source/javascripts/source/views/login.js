d56144edd52348519c64fde2fb0a8f2348e38494
Killian Le Clainche <kleclain@purdue.edu>
2017-09-28 11:44:38 -0500

    Removed background animation, Logo looks meh. Updates to CSS and addition of the login and register buttons

--- source/javascripts/source/views/login.js
+++ source/javascripts/source/views/login.js
@@ -10,18 +10,29 @@ const loginView = {
 					"<div class='form--label'><img src='./images/logo.png' alt='VENote' class='login--logo-image' width='600'/></div>" +
 					"<div class='form--text'><input name='username' type='text' placeholder='Username' data-required></div>" +
 					"<div class='form--text'><input name='password' type='password' placeholder='Password' data-required></div>" +
+					"<div class='form--half'>" +
+						"<button type='submit' title='Login' class='login button button--primary button--normal' style='background:forestgreen; margin-top:30px'>" +
+							"<span>Login</span>" +
+						"</button>" +
+					"</div>" +
+					"<div class='form--half'>" +
+						"<button type='submit' title='Register' class='register button button--primary button--normal' style='background:indianred; margin-top:30px'>" +
+							"<span>Register</span>" +
+						"</button>" +
+					"</div>" +
 				"</form>" +
 			"</div>"));
 
-		body.animate({"background-color": "#557FBE"}, 200, function() {
-
-		});
+		$("button[type='submit']").on("click", function(e, e1, e2) {
+			console.log(e);
+			console.log(e1);
+			console.log(e2);
+		})
 
 		$("input").focus(function()
 		{
 			console.log($(this));
 		})
-
 	},
 
 	transition() {
