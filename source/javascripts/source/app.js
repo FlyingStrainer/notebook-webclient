ec3458698d4f2417babbe5e7b1a2671f03c8740f
Chad Stucky <chad@rustymailbox.com>
2017-09-28 17:32:47 -0400

    create initial classes for notebook and page

--- source/javascripts/source/app.js
+++ source/javascripts/source/app.js
@@ -3,3 +3,35 @@ import loginView from "./views/login.js";
 $(document).ready(function() {
 	loginView.init(React, ReactDOM);
 });
+
+// Models
+var Page = function(){};
+Page.prototype = 
+{
+  id: null,
+  signiture: null,
+  creator: null
+};
+
+var Notebook = function(){};
+Notebook.prototype = 
+{
+  id: null,
+  pages: null,
+  creator: null
+};
+
+//Example of how to make a 'class' and instantiate an object
+var myClass = function(){};
+myClass.prototype = {
+    someProperty: null,
+    someOtherProperty: 0,
+
+    doSomething: function (msg) {
+      this.someProperty = msg;
+      alert(this.someProperty);
+    }
+};
+
+var myClassObj = new myClass();
+myClassObject.doSomething("Hello World");
