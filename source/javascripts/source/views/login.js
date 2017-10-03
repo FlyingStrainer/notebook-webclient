c602ab72d7384e53a80779cf3abb43a39add5931
Killian Le Clainche <kleclain@purdue.edu>
2017-10-02 20:30:46 -0500

    Setting up app.js to handle multiple views + training Ryan for usae of the current system. Created Label object in form.js for Form labels like images and text

--- source/javascripts/source/views/login.js
+++ source/javascripts/source/views/login.js
@@ -5,34 +5,87 @@ const loginView = {
 	init() {
 		const body = $("body");
 
-		body.append($("<div class='form-container form-style'>" +
+		body.append($("<div class='form-container form-style login'>" +
 				"<form method='post'>" +
 					"<div class='form--label'><img src='./images/logo.png' alt='VENote' class='login--logo-image' width='600'/></div>" +
-					"<div class='form--text'><input name='username' type='text' placeholder='Username' data-required></div>" +
-					"<div class='form--text'><input name='password' type='password' placeholder='Password' data-required></div>" +
+					"<div class='form--text login--id'><input name='username' type='text' placeholder='Username' data-required></div>" +
+					"<div class='form--text login--id'><input name='password' type='password' placeholder='Password' data-required></div>" +
+					"<div class='form--text register--id' style='display:none;'><input name='username' type='text' placeholder='Username' data-required></div>" +
+					"<div class='form--text register--id' style='display:none;'><input name='password' type='password' placeholder='Password' data-required></div>" +
+					"<div class='form--text register--id' style='display:none;'><input name='confirmpassword' type='password' placeholder='Confirm Password' data-required></div>" +
+					"<div class='form--text register--id' style='display:none;'><input name='companyid' type='number' placeholder='Company ID' data-required></div>" +
 					"<div class='form--half'>" +
-						"<button type='submit' title='Login' class='login button button--primary button--normal' style='background:forestgreen; margin-top:30px'>" +
+						"<button type='submit' title='Login' class='login button button--primary button--normal' style='background:forestgreen; margin-top:20px'>" +
 							"<span>Login</span>" +
 						"</button>" +
 					"</div>" +
 					"<div class='form--half'>" +
-						"<button type='submit' title='Register' class='register button button--primary button--normal' style='background:indianred; margin-top:30px'>" +
+						"<button type='submit' title='Register' class='register button button--primary button--normal' style='background:indianred; margin-top:20px'>" +
 							"<span>Register</span>" +
 						"</button>" +
 					"</div>" +
 				"</form>" +
 			"</div>"));
 
-		$("button[type='submit']").on("click", function(e, e1, e2) {
-			console.log(e);
-			console.log(e1);
-			console.log(e2);
-		})
+		const loginfields = body.find(".login--id");
+		const registerfields = body.find(".register--id");
 
-		$("input").focus(function()
-		{
-			console.log($(this));
-		})
+		let loginMode = 0;
+
+		let loginTransition = 1;
+
+		$("button[type='submit']").on("click", function(e, e1, e2) {
+			if(loginTransition === 1)
+			{
+				if(loginMode === 0)
+				{
+					if($(this).attr("title") === "Register")
+					{
+						loginMode = 1;
+						loginfields.hide(500, function() {
+							if($(this).is(loginfields.first()))
+							{
+								registerfields.show(500, function() {
+									if($(this).is(registerfields.first()))
+									{
+
+									}
+								});
+							}
+						});
+					}
+					else
+					{
+						//DO TRANSITION TO NEXT PAGE
+					}
+				}
+				else
+				{
+					if($(this).attr("title") === "Login")
+					{
+						loginMode = 0;
+						registerfields.hide(500, function()
+						{
+							if ($(this).is(registerfields.first()))
+							{
+								loginfields.show(500, function ()
+								{
+									if ($(this).is(loginfields.first()))
+									{
+
+									}
+								});
+							}
+						});
+					}
+					else
+					{
+						//DO TRANSITION TO NEXT PAGE
+					}
+				}
+			}
+			e.preventDefault();
+		});
 	},
 
 	transition() {
