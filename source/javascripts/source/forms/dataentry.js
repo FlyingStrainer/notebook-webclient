5e7eb412e1d11717349c08e2eb077f18ebc24074
RyanNavillus <ryanpnavillus@gmail.com>
2017-10-02 21:44:21 -0400

    Format data entry

--- source/javascripts/source/forms/dataentry.js
+++ source/javascripts/source/forms/dataentry.js
@@ -2,18 +2,25 @@ import * as form from "./form.js";
 //import React from "./../../lib/react.js"; 
 //import ReactDOM from "./../../lib/react-dom.js";
 export * from "./form.js";
-
+var rootObject;
 const dataEntryForm = {
 	init(root) {
+		rootObject = root;
 		ReactDOM.render(
-			<div>
+			<div id="Title">
 				<h1>DataEntry</h1>
 				<script>console.log("Hi")</script>
+				<script>addTextField()</script>
 			</div>
-		, root
+			, root
 		);
-		console.log("Hi");
 	}
 };
+
+function addTextField() {
+	return <div>
+		<h1>TextEntry</h1>
+	</div>;
+}
 
 export default dataEntryForm;
