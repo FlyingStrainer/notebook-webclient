5399542f8c6ca3276caac01d0d6dfa0fb67fbf35
RyanNavillus <ryanpnavillus@gmail.com>
2017-10-04 04:44:36 -0400

    Add image display to data entry form

--- source/javascripts/source/forms/dataentry.js
+++ source/javascripts/source/forms/dataentry.js
@@ -18,7 +18,25 @@ class DataEntryForm extends React.Component {
 class DataEntryFields extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {hasImage: false, file: [], imgSrc: null};
+		this.fileSelected = this.fileSelected.bind(this);
+	}
+	
+	fileSelected(input) {
+		console.log("File Selected:");
+		console.log(input.target.value);
+		if (input.target.value) {
+			var file = this.refs.file.files[0];
+			var reader = new FileReader();
+			console.log("out");
+			reader.onloadend = function (e) {
+				console.log("in");
+				this.setState({
+					imgSrc: [reader.result]
+				})
+			}.bind(this);
+			var url = reader.readAsDataURL(file);
+		}
 	}
 
 	render() {
@@ -26,7 +44,8 @@ class DataEntryFields extends React.Component {
 			Describe your work:<br />
 			<textarea className="data-form" id="text-box"></textarea><br /><br />
 			Include an image:<br />
-			<input type="file" className="data-form" id="image-upload"/><br /><br />
+			<input type="file" ref="file" className="data-form" id="image-upload" accept="image/*" onChange={(event)=>{this.fileSelected(event)}} /><br /><br />
+			<img className="data-form" id="image" src={this.state.imgSrc} />
 			Caption the image:<br />
 			<textarea className="data-form" id="caption-box"></textarea><br /><br />
 		
