537fc12e18d07c12bfae6f0ce56d6b8d496d4b49
RyanNavillus <ryanpnavillus@gmail.com>
2017-10-04 22:30:30 -0400

    Add placeholder API call to submit data entry

--- source/javascripts/source/forms/dataentry.js
+++ source/javascripts/source/forms/dataentry.js
@@ -39,17 +39,32 @@ class DataEntryFields extends React.Component {
 		}
 	}
 
+	submitPage() {
+		console.log("Hai");
+		fetch('', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'appication/json'
+			},
+			body: JSON.stringify({
+				param1: 'value1',
+				param2: 'value2'
+			})
+		});
+	}
+
 	render() {
 		return (<form className="data-form" id="form">
 			Describe your work:<br />
 			<textarea className="data-form" id="text-box"></textarea><br /><br />
 			Include an image:<br />
 			<input type="file" ref="file" className="data-form" id="image-upload" accept="image/*" onChange={(event)=>{this.fileSelected(event)}} /><br /><br />
-			<img className="data-form" id="image" src={this.state.imgSrc} />
+			<img className="data-form" id="image" src={this.state.imgSrc} /><br /><br />
 			Caption the image:<br />
 			<textarea className="data-form" id="caption-box"></textarea><br /><br />
 		
-			<input className="data-form" id="submit-button" type="submit" /><br /><br />
+			<input className="data-form" id="submit-button" type="button" value="Submit" onClick={this.submitPage} /><br /><br />
 		</form>);	
 	}
 }
