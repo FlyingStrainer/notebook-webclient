7ea170d6538ed83ff1d2519af6a13255473dda7f
RyanNavillus <ryanpnavillus@gmail.com>
2017-10-05 02:24:09 -0400

    Create local object to store data entry and convert to JSON

---
+++ source/javascripts/source/models/dataentry.js
@@ -0,0 +1,22 @@
+export default DataEntry;
+
+class DataEntry {
+	constructor(text, image, caption, author) {
+		this.text = text;
+		this.image = image;
+		this.caption = caption;
+		this.date_created = new Date();
+		this.author = author;
+	}
+
+	toJSON() {
+		var json = '{'
+			+ `"text": ${this.text}`
+			+ `"image": ${this.image}`
+			+ `"caption": ${this.caption}`
+			+ `'date_created': ${this.date_created}`
+			+ `"author_id": ${this.author.id}`
+		+ '}';
+		return json;
+	}
+}
