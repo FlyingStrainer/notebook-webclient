719a1cb053ae5a82d8715d340006234172e06260
RyanNavillus <ryanpnavillus@gmail.com>
2017-10-27 06:01:24 -0400

    Complete refactor

--- source/javascripts/source/forms/dataentry.js
+++ source/javascripts/source/forms/dataentry.js
@@ -27,45 +27,47 @@ class DataEntryFields extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {hasImage: false, file: [], imgSrc: null};
-		this.fileSelected = this.fileSelected.bind(this);
 		this.dataEntry = props.dataEntry;
 		this.submitPage = this.submitPage.bind(this);
-		this.fileSelected = this.fileSelected.bind(this);
 		this.submitCallback = props.submitCallback;
 		this.author=props.author;
+
+		this.textChanged = this.textChanged.bind(this);
+		this.captionChanged = this.captionChanged.bind(this);
+		this.dateChanged = this.dateChanged.bind(this);
+		this.tagsChanged = this.tagsChanged.bind(this);
+		this.authorChanged = this.authorChanged.bind(this);
+		this.imageChanged = this.imageChanged.bind(this);
 	}
 	
-	// Select file from image selector
-	fileSelected(input) {
-		console.log("File Selected:");
-		console.log(input.target.value);
-		if (input.target.value) {
-			var file = this.refs.file.files[0];
-			var reader = new FileReader();
-			reader.onloadend = function (e) {
-				this.dataEntry.image = reader.result;
-				this.setState({
-					imgSrc: [reader.result]
-				})
-			}.bind(this);
-			var url = reader.readAsDataURL(file);
-		}
+	textChanged(e) {
+		this.dataEntry.text = e.target.value;
 	}
+
+	captionChanged(e) {
+		this.dataEntry.caption = e.target.value;
+	}
+
+	dateChanged(e) {
+		this.dataEntry.date = e.target.value	
+	}
+
+	tagsChanged(e) {
+		this.dataEntry.tags = e.target.value;
+	}
+
+	authorChanged(author) {
+		this.dataEntry.author = author;
+	}
+
+	imageChanged(image) {
+		this.dataEntry.image = image;
+	}	
 
 	// submit page to api
 	submitPage() {
-		var checkbox = document.getElementById("checkbox");
-		if (!checkbox.checked)
-		{
-			alert("You must check the box before submitting.");
-			return;
-		}
 		console.log(this.dataEntry);
-		this.dataEntry.text = document.getElementById("text-box").value;
-		this.dataEntry.caption = document.getElementById("caption-box").value;
 		this.dataEntry.date_created = new Date();
-		this.dataEntry.tags = document.getElementById("tag-box").value;
-		this.dataEntry.author = "Author";
 		//Need to set author
 
 		console.log(this.dataEntry);
@@ -84,33 +86,23 @@ class DataEntryFields extends React.Component {
 		}
 
 		// Clear form for future use
-		document.getElementById("text-box").value = "";
-		document.getElementById("caption-box").value = "";
-		document.getElementById("tag-box").value = "";
-		document.getElementById("checkbox").checked = false;
 		this.dataEntry = new DataEntryModel("", "", "", "", "");
 	}
 
 	render() {
 		return (<div className="data-form" id="form-div">
 				<form className="data-form" id="form">
-					<TextInput label="Describe your work:" />
+					<TextInput label="Describe your work:" textHandler={this.textChanged} />
+					< br />
 
 					Include an image:<br />
-					<input type="file" ref="file" className="data-form" id="image-upload" accept="image/*" onChange={(event)=>{this.fileSelected(event)}} /><br />
-					<img className="data-form" id="image" src={this.state.imgSrc} /><br />
+					<ImageInput imageHandler={this.imageChanged} />
 
-					<TextInput label="Caption the image:" />
-
-					Add image tags:<br />
-					(Write tags as a comma separated list, for example: "Wheels, Drive Train, Movement")<br />
-					<textarea className="data-form" id="tag-box"></textarea><br /><br />
-					<TextInput label="Add image tags:\n(Write tags as a comma separated list, for example: 'Wheels, Drive Train, Movement')" />
-					<label>	
-						<input className="data-form" id="checkbox" type="checkbox" value="Hi"/>
-						By checking this you confirm the accuracy of this entry.<br /><br />
-					</label>
-					<input className="data-form" id="submit-button" type="button" value="Submit" onClick={this.submitPage} /><br /><br />
+					<TextInput label="Caption the image:" textHandler={this.captionChanged} />
+					<br />
+					<TextInput label="Add image tags:\n(Write tags as a comma separated list, for example: 'Wheels, Drive Train, Movement')" textHandler={this.tagsChanged}/>	
+					<br />
+					<SubmitButton message="By checking this you confirm the accuracy of this entry." label="Submit" submissionHandler={this.submitPage}/>
 				</form>
 			</div>);	
 	}
@@ -121,6 +113,7 @@ class TextInput extends React.Component {
 	constructor(props) {
 		super(props);
 		this.label = props.label
+		this.textHandler = props.textHandler;
 	}
 	
 	render() {
@@ -129,28 +122,74 @@ class TextInput extends React.Component {
 		        {text.split("\\n").map(i => {
           			return <div>{i}</div>;
         		})}
-			<textarea className="data-form" id="text-box"></textarea><br />
+			<textarea onChange={this.textHandler}></textarea><br />
 		</div>
-
 	}
 }
 
 class ImageInput extends React.Component {
 	constructor(props) {
 		super(props);
+		this.imageHandler = props.imageHandler;
+		this.state = {imgSrc: []}
+		this.fileSelected = this.fileSelected.bind(this);
+	}
+
+	// Select file from image selector
+	fileSelected(input) {
+		console.log("File Selected:");
+		console.log(input.target.value);
+		if (input.target.value) {
+			var file = this.refs.file.files[0];
+			var reader = new FileReader();
+			reader.onloadend = function (e) {
+				console.log("Hi");
+				this.imageHandler(reader.result);
+				this.setState({
+					imgSrc: [reader.result]
+				})
+			}.bind(this);
+			var url = reader.readAsDataURL(file);
+		}
 	}
 	
 	render() {
+		return <div>
+			<input type="file" ref="file" className="data-form" id="image-upload" accept="image/*" onChange={(event)=>{this.fileSelected(event)}} /><br />
+			<img className="data-form" id="image" src={this.state.imgSrc} /><br />
 
+		</div>
 	}
 }
 
 class SubmitButton extends React.Component {
 	constructor(props) {
 		super(props);
+		this.message = props.message;
+		this.label = props.label;
+		this.submitPage = props.submissionHandler;
+		this.submit = this.submit.bind(this);
 	}
 	
+	submit() {
+		var checkbox = document.getElementById("checkbox");
+		if (!checkbox.checked)
+		{
+			alert("You must check the box before submitting.");
+		}
+		else
+		{
+			this.submitPage();
+			checkbox.checked = false;
+		}
+	}
 	render() {
-
+		return <div>
+			<label>	
+				<input id="checkbox" type="checkbox"/>
+				{this.message} <br /><br />
+			</label>
+			<input type="button" value={this.label} onClick={this.submit} /><br />
+		</div>
 	}
 }
