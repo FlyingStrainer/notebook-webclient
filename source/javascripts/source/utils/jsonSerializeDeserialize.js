5a8ceca75169d653c7a849c57aaf69d0e7b729d2
Chad Stucky <chad@rustymailbox.com>
2017-10-30 20:09:45 -0400

    deserialize user object

--- source/javascripts/source/utils/jsonSerializeDeserialize.js
+++ source/javascripts/source/utils/jsonSerializeDeserialize.js
@@ -1,8 +1,12 @@
+import User from "../models/user.js"; 
+
 // Functions to serialize and deserialize json into model objects live here
 
 function userFromJson( json )
 {
-
+  var userFromJson = JSON.parse(json);
+  let user = Object.assign( new User, userFromJson );
+  return user;
 }
 
 function userToJson( user )
