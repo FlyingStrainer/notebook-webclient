988251fdb610956be46d94750995ca927396ee07
Chad Stucky <chad@rustymailbox.com>
2017-11-03 06:31:40 -0400

    match user model with what backend guys have

--- source/javascripts/source/models/user.js
+++ source/javascripts/source/models/user.js
@@ -1,8 +1,13 @@
 export default class User 
 {
-  constructor(user_hash, permissions)
+  constructor(user_hash, permissions, company_name, notebooks, roles)
   {
     this.user_hash = user_hash;
+    this.company_name = company_name;
+
+    // Arrays
     this.permissions = permissions;
+    this.notebooks = notebooks;
+    this.roles = roles;
   }
 }
