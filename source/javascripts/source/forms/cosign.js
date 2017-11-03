44e3b0b03695941ce707ca15b09f394882c0954a
Navillus <rps214@verizon.net>
2017-11-03 14:26:43 -0400

    Add cosign API call

--- source/javascripts/source/forms/cosign.js
+++ source/javascripts/source/forms/cosign.js
@@ -7,19 +7,42 @@ export class CosignEntryForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
+		this.author = props.author;
 		this.cancelCallback = props.cancelCallback;
 		this.submitCallback = props.submitCallback;
+		this.cosign = this.cosign.bind(this);
+	}
+
+	cosign() {
+		console.log("cosign");
+
+		fetch('api/cosignEntry', {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: {
+				user_hash: this.author ? this.author : "",
+				notebook_hash: "",
+				entry_hash: ""
+			}
+		});	
+
+		if (this.submitCallback) {
+			this.submitCallback();
+		}
 	}
 
 	render() {
-		return <div>
+		return 	<div>
 				<div className="forms header" id="container">
 					<h1 className="forms header" id="header-text">Sign Entry</h1>
 					<input className="forms header" id="cancel-button" type="button" value="Cancel" onClick={this.cancelCallback}/>
 				</div>
 				<br />
 				<br />
-				<SignEntryFields submitCallback={this.submitCallback} author={this.author}/>
+				<SignEntryFields submitCallback={this.cosign} author={this.author}/>
 			</div>;
 	}
 }
